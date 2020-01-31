04758f9f3fc30ffadbdc710816ac94315408cd16
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-31 15:54:27 -0800

    fix issues with adding toppings property to pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,6 @@
-//User Interface Logic
-var pizza = new Pizza();
-
-$(document).ready(function(){
-  $("form").submit(function(){
-    event.preventDefault();
-
-    var toppingNames = [];
-   $("input:checkbox[name=select-toppings]:checked").each(function(){
-      toppingName = $(this).val();
-      toppingNames.push(toppingName);
-      console.log(toppingNames);
-   });
-    var size = $("select").val();
-    $("span.size-output").text(size);
-    
-    pizza.addTopping(toppingNames);
-    console.log(pizza);
-    
-    // var price = calculatePrice(toppings, size);
-
-    // var newPizza = new Pizza(toppings, size, price);
-    // console.log(newPizza)
-  });
-});
-
 //Business logic for Pizza constructor
 function Pizza(toppings, size, price){
-  this.toppings = [];
+  this.toppings = toppings;
   this.size = size;
   this.price = price;
   // this.currentToppingId = 0;
@@ -38,6 +12,13 @@ function Pizza(toppings, size, price){
 //     return this.price;
 //   }
 // }
+
+
+// Pizza.prototype.addTopping = function(toppingNames){
+//   // toppings.toppingId = this.assignToppingId();
+//   this.toppings.push(toppingNames);
+// }
+
 
 function calculatePrice(toppings, size) {
   // if(this.toppings.length >=2 && this.size === "50cm"){
@@ -53,9 +34,34 @@ function calculatePrice(toppings, size) {
 //   this.currentToppingId ++;
 //   return this.currentToppingId;
 // }
+//User Interface Logic
 
 
-Pizza.prototype.addTopping = function(toppingNames){
-  // toppings.toppingId = this.assignToppingId();
-  this.toppings.push(toppingNames);
-}
+$(document).ready(function(){
+  $("form").submit(function(){
+    event.preventDefault();
+
+    var toppingNames = [];
+    $("input:checkbox[name=select-toppings]:checked").each(function(){
+      toppingName = $(this).val();
+      toppingNames.push(toppingName);
+      console.log(toppingNames);
+    });
+    
+
+    var size = $("select").val();
+    $("span.size-output").text(size);
+    
+    var price;
+
+    var pizza = new Pizza(toppingNames, size, price);
+    console.log(pizza);
+    
+    // var price = calculatePrice(toppings, size);
+
+    // var newPizza = new Pizza(toppings, size, price);
+    // console.log(newPizza)
+  });
+});
+
+
