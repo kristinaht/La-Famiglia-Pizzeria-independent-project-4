2f3c6b5dd849a23ad10c8bc963bd44de2fc81c12
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-31 13:28:07 -0800

    attempt to calculate pizza price

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,35 @@ $(document).ready(function(){
       return toppings;
     });
     console.log(toppings);
+    var price = calculatePrice(toppings, size);
+    var newPizza = new Pizza(toppings, size, price);
+    console.log(newPizza)
   });
 });
 
 //Business logic for Pizza constructor
-function Pizza(toppings, size){
-  this.toppings = toppings;
+function Pizza(toppings, size, price){
+  this.toppings = [];
   this.size = size;
+  this.price = price;
   // this.currentToppingId = 0;
 }
+
+// Pizza.prototype.calculatePrice = function(toppings, size) {
+//   if(this.toppings.length >=2 && this.size === "50cm"){
+//     this.price = 20;
+//     return this.price;
+//   }
+// }
+
+function calculatePrice(toppings, size) {
+  // if(this.toppings.length >=2 && this.size === "50cm"){
+    if(size === "50cm")
+    this.price = 20;
+    return this.price;
+  }
+
+
 
 // Pizza.prototype.addTopping = function(){
 //   toppings.toppingId = this.assignToppingId();
