907b398a46e74e11f512680994821863a847d3b5
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-31 09:58:16 -0800

    modify html and add logic to js file to test second sped

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,10 @@ $(document).ready(function(){
     event.preventDefault();
     var size = $("select").val();
     $("span.size-output").text(size);
-  })
+    $("input:checkbox[name=select-toppings]:checked").each(function(){
+      var toppings = $(this).val();
+      $("span.toppings-output").append(toppings + " ");
+    });
+
+  });
 });
