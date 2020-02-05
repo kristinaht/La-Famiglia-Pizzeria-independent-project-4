966f7467db0dc92b3ad3aa828d8d0e205051fa21
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-02-04 20:50:07 -0800

    remove global var pizza, declare var basePrice, reformat calculatePrice prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,24 @@ function Pizza(toppings, size){
 }
 
 Pizza.prototype.calculatePrice = function(toppings, size) {
- basePrice = 10;
+ var basePrice = 10;
   if(size === "small" && this.toppings.length >2){
     basePrice +=2;
-    return basePrice;
   }else if(size === "medium" && this.toppings.length <=2){
     basePrice +=3;
-    return basePrice;
   }else if(size === "medium" && this.toppings.length >2){
     basePrice +=4;
-    return basePrice;
   }else if(size === "large" && this.toppings.length === 1){
     basePrice +=5;
-    return basePrice;
   }else if(size === "large" && this.toppings.length >1){
     basePrice += 10;
-    return basePrice;
   }else{
-    return basePrice;
+    basePrice;
   }
+  return basePrice;
 }
 
 //User Interface logic
- var pizza;
-
 $(document).ready(function(){    
   $("button.btn-block").click(function(){
     $("div.form-page").show();
@@ -45,7 +39,7 @@ $(document).ready(function(){
     var size = $("select").val();
     $("span.size-output").text(size);
     
-    pizza = new Pizza(toppingNames, size);
+    var pizza = new Pizza(toppingNames, size);
     var price = pizza.calculatePrice(toppingNames, size);
     $("span.price-output").text(price);
   });
